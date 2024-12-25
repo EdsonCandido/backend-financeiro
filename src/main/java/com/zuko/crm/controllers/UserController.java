09b384dc71a5d9c03761dccc7bdd9b421d8c1d83
Edson Cândido <edson.c.a.j@hotmail.com>
2024-12-25 19:52:01 -0300

    [feat] complete crud users

--- src/main/java/com/zuko/crm/controllers/UserController.java
+++ src/main/java/com/zuko/crm/controllers/UserController.java
@@ -1,15 +1,15 @@
 package com.zuko.crm.controllers;
 
 import com.zuko.crm.dto.request.CreateUserRequestDTO;
+import com.zuko.crm.dto.request.UpdateUserRequestDTO;
 import com.zuko.crm.entities.UserEntity;
 import com.zuko.crm.repositorys.UserRepository;
 import com.zuko.crm.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -20,9 +20,29 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/")
+    public ResponseEntity<List<UserEntity>> findAll(){
+        var output = userService.findAll();
+        return ResponseEntity.ok(output);
+    }
+
     @PostMapping("/")
     public ResponseEntity<UserEntity> create(@RequestBody CreateUserRequestDTO dto){
-        var output = this.userService.createUser(dto);
+        var output = userService.createUser(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(output);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<UserEntity> update(@RequestBody UpdateUserRequestDTO dto){
+        var output = userService.update(dto);
+
+        return ResponseEntity.ok(output);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserEntity> changeActive(@PathVariable("id") long id){
+        var output = userService.changeActive(id);
+
+        return ResponseEntity.ok(output);
     }
 }
