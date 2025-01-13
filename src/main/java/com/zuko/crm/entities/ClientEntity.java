83d1f79a1683637c9e4bbe378c1d920598e7e449
Edson Cândido <edson.c.a.j@hotmail.com>
2025-01-12 22:52:56 -0300

    [feat] create entity

---
+++ src/main/java/com/zuko/crm/entities/ClientEntity.java
@@ -0,0 +1,83 @@
+package com.zuko.crm.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity(name = "tb_clients")
+public class ClientEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "client_id")
+    private Long clientId ;
+
+    @Column(name = "corporate_reason")
+    private String corporateReason;
+
+    @Column(name = "fantasy_name")
+    private String fantasyName;
+
+    @Column(name = "is_active")
+    @ColumnDefault("true")
+    private Boolean isActive;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getCorporateReason() {
+        return corporateReason;
+    }
+
+    public void setCorporateReason(String corporateReason) {
+        this.corporateReason = corporateReason;
+    }
+
+    public String getFantasyName() {
+        return fantasyName;
+    }
+
+    public void setFantasyName(String fantasyName) {
+        this.fantasyName = fantasyName;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
